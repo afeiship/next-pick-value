1bf6cdfcee7c5252e0ee7e6248468c8d2da8cba3
afeiship <1290657123@qq.com>
2018-11-29 13:08:21 +0800

    add: prettier

--- dist/next-pick-value.js
+++ dist/next-pick-value.js
@@ -3,10 +3,10 @@
   var global = global || window || self || Function('return this')();
   var nx = global.nx || require('next-js-core2');
 
-  nx.pickValue = function (inList, inValue) {
+  nx.pickValue = function (inArray, inValue) {
     var current = null;
-    nx.each( inList, function(_, item ){
-      if( item.key === inValue ){
+    nx.each(inArray, function (_, item) {
+      if (item.key === inValue) {
         current = item;
         return nx.BREAKER;
       }
